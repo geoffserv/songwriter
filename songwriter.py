67905d37fc0b9bc9e69830bbc513a6cf80f7e388
geoffserv <gtraugott@gmail.com>
2021-08-26 22:22:05 -0500

    mussin around

--- songwriter.py
+++ songwriter.py
@@ -36,7 +36,32 @@ if __name__ == "__main__":
 	sourceTextBlob = sourceTextFile.read().replace('\n', ' ')
 	sourceTextFile.close()
 
-	sourceSentences = re.split('[,.]', sourceTextBlob) # Break it apart at every comma or period
+	sourceSentences = re.split('[,.!]', sourceTextBlob) # Break it apart at every comma,period,ep
 	
 	for sourceSentence in sourceSentences:
-		
+
+			if len(sourceSentence) > 1: # Empty elements get caught up in here and crash, look for >1 element sentences only
+
+				sourceSentence = sourceSentence.strip()
+				sourceSentenceWords = sourceSentence.split()
+				lastWord = sourceSentenceWords[-1]
+
+				sourceSentenceSyllables = 0
+				for sourceSentenceWord in sourceSentenceWords:
+					sourceSentenceSyllables += syllables.estimate(sourceSentenceWord)
+
+				#print("DEBUG -- sentence:", sourceSentence, " --- lastword:", lastWord, " --- syllables:", sourceSentenceSyllables)
+
+				if not lastWord in lyricDict:
+					# Haven't encountered this last word before, so build a lyricDict entry for this last word
+					newLyric = {lastWord:[[sourceSentence, sourceSentenceSyllables]]}
+					lyricDict.update(newLyric)
+				#else:
+					#print("DEBUG: found key in lyricDict:", lastWord)
+					#print("lyricDict[lastWord][0]:",lyricDict[lastWord][0])
+					#if sourceSentence not in lyricDict[lastWord][0]:
+					#	lyricDict[lastWord][0].append([sourceSentence, sourceSentenceSyllables])
+					#print(lyricDict[lastWord])
+					#print(lyricDict[lastWord][1])
+					#lyricDict[lastWord][1] += 1
+	print(lyricDict)
