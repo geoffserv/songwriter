66537ef3370d6cf7fd53c158c2f22c63b5883a99
geoffserv <gtraugott@gmail.com>
2021-08-27 19:38:15 -0500

    Doc cleanup, example txt cleanup

--- songwriter.py
+++ songwriter.py
@@ -8,21 +8,35 @@ import time
 import random
 
 # lyricDict will contain a lyric dictionary built from the source text
-# The key will be the last word of each line
-# The value will be a list:
-#   element 0, a list of every line ending in that word
-#      element 0, the line text itself
-#      element 1, the number of syllables estimated in that line
-#   element 1, a list of every known rhyme with that word:
-#      element 0, the rhyme word
-#      element 1, the type of rhyme:
+#
+# Currently, source text is split by one of . , !
+#
+# lyrictDict key is the last word of each line
+#
+# lyricDict[lastWord]
+#
+# The structure is:
+#   lyricDict[lastWord][0]         a list of lines ending in that word
+#   lyricDict[lastWord][0][n]      a list containing the line of text and sentence syllable estimation
+#   lyricDict[lastWord][0][n][0]   the line of text
+#   lyricDict[lastWord][0][n][1]   the number of syllables estimated in that line
+#   lyricDict[lastWord][1]         a list of every known rhyme of the lastWord, along with the rhyme type
+#   lyricDict[lastWord][1][0]      a list of every known rhyme of the lastWord
+#   lyricDict[lastWord][1][1]      the type of rhyme:
+#
 #                 0 = Perfect rhyme (FOB, DOG)
+#
+#                 Currently, only 0 = Perfect rhyme is implemented
+#
+#                 If it's interesting, TODO would be to implement:
+#
 #                 1 = same vowels and consonants of the same type regardless of voicing (HAWK, DOG)
 #                 2 = same vowels and consonants as well as any extra consonants (DUDES, DUES)
 #                 3 = same vowels and a subset of the same consonants (DUDE, DO)
 #                 4 = same vowels and some of the same consonants, with some swapped for other consonants (FACTOR, FASTER)
 #                 5 = same vowels and arbitrary consonants (CASH, CATS)
 #                 6 = not the same vowels but the same consonants (CAT, BOT)
+#
 #                 so, the higher this index, the less "rhymey" is it, kinda
 
 lyricDict = {}
@@ -32,7 +46,7 @@ lyricDict = {}
 sourceFile = "bible.txt"
 
 # Verses to generate
-countVersesToGenerate = 10
+countVersesToGenerate = 25
 
 # Some debug counters
 
@@ -48,7 +62,6 @@ debugProcEndTime = 0
 
 if __name__ == "__main__":
 
-	# Song Building
 	# PROOF OF CONCEPT
 	# Rhyme and meter scheme:
 	# A (approx 9 syllables)
@@ -60,9 +73,10 @@ if __name__ == "__main__":
 
 	songMeterPadding = 1 # Give or take this many syllables.  The syllable estimator is also kinda inaccurate
 
-	# Set the meter now.  This will save an enormous amount of processing from discarding non-matching lines.
+	# Set the meter now.  This will save an enormous amount of processing by discarding non-meter-matching 
+	# lines prior to all the syllable estimation and rhyme calculation
 
-	print("Song Meter- A line:", songMeter['A'], "syllables; B line:", songMeter['B'], "syllables")
+	print("INFO- Song Meter- A line:", songMeter['A'], "syllables; B line:", songMeter['B'], "syllables")
 
 	debugProcStartTime = time.time()
 
@@ -70,20 +84,19 @@ if __name__ == "__main__":
 
 	print("INFO- Opening file for processing:", sourceFile)
 
-	# Read the entire source file in to a string for later splitting and manipulation
+	# Read the entire source file in to a string for splitting and manipulation
 	# This is very inefficient but hello-world quality for now
 	# Read in and kill the newlines because they're unimportant
-	sourceTextFile = open(sourceFile, 'r') # some public domain text as a test case
+	sourceTextFile = open(sourceFile, 'r')
 	sourceTextBlob = sourceTextFile.read().replace('\n', ' ')
 	sourceTextFile.close()
 
 	sourceSentences = re.split('[,.!]', sourceTextBlob) # Break it apart at every comma,period,ep
 
-	debugTotalLinesSeen = len(sourceSentences)
-
 	print("INFO- Deduplicating sentence list ...")
-	sourceSentences = list(dict.fromkeys(sourceSentences))
 
+	debugTotalLinesSeen = len(sourceSentences)
+	sourceSentences = list(dict.fromkeys(sourceSentences))
 	debugTotalDiscardedLines = debugTotalLinesSeen - len(sourceSentences)
 
 	print("INFO- Building lyric dictionary ...")
@@ -96,6 +109,7 @@ if __name__ == "__main__":
 
 				sourceSentence = sourceSentence.strip()
 				sourceSentenceWords = sourceSentence.split()
+				if not sourceSentenceWords: continue # Lil' hack for now, TODO refactor input checking
 				lastWord = sourceSentenceWords[-1]
 
 				debugTotalWordsProcessed += len(sourceSentenceWords)
@@ -107,7 +121,7 @@ if __name__ == "__main__":
 					sourceSentenceSyllables += syllables.estimate(sourceSentenceWord)
 					debugTotalSyllablesSeen += sourceSentenceSyllables
 
-				# Discard all lines that don't match the A or B meter plus-or-minus the meter padding
+				# Skip any lines that don't match the A or B meter plus-or-minus the meter padding
 				if not ( 
              ( (sourceSentenceSyllables < songMeter['A'] + songMeterPadding) and
              (sourceSentenceSyllables > songMeter['A'] - songMeterPadding) ) or
@@ -126,9 +140,9 @@ if __name__ == "__main__":
 					try:
 						# What comes back is a dictionary of syllable counts with corresponding lists of rhyme words.
 						# Kinda don't care about rhyme word syllable counts right now, so just collapse this to values() only
-						sourceSentenceRhymeList1 = rhyme.get_perfect_rhymes(lastWord).values()
+						sourceSentenceRhymeList = rhyme.get_perfect_rhymes(lastWord).values()
 						# Now this is a list of lists, better to collapse to a single list of all the words
-						sourceSentenceRhymeList1 = [item for sublist in sourceSentenceRhymeList1 for item in sublist]
+						sourceSentenceRhymeList = [item for sublist in sourceSentenceRhymeList for item in sublist]
 						# Maybe someday do fancy things with lastWord syllable count ...
 
 					except KeyError:
@@ -143,7 +157,7 @@ if __name__ == "__main__":
                                    [sourceSentence, sourceSentenceSyllables]
                                  ],
                                  [
-                                   [sourceSentenceRhymeList1, 0]
+                                   [sourceSentenceRhymeList, 0]
                                  ]
                                ]
                      }
@@ -156,6 +170,7 @@ if __name__ == "__main__":
 					debugTotalUniqueLines += 1
 	
 	debugProcEndTime = time.time()
+
 	print("INFO- Completed building lyric dictionary in", debugProcEndTime-debugProcStartTime, "seconds")
 	print("INFO- Total Lines Processed:", debugTotalLinesProcessed)
 	print("INFO- Total Words Processed:", debugTotalWordsProcessed)
@@ -168,24 +183,23 @@ if __name__ == "__main__":
 
 	countVersesSoFar = 0 # How many verses we've generated so far
 
-	print("INFO- Beginning generation of", countVersesToGenerate, "verses")
+	print("INFO- Beginning generation of", countVersesToGenerate, "verses ...")
 
 	while (countVersesSoFar < countVersesToGenerate):
 
-		lyricSearchLimit = 1000 # Search this many times until giving up, otherwise may loop forever.  Gross hack but ok 4now
+		lyricSearchLimit = 1000 # Search this many times until giving up, otherwise may loop forever?  Gross hack but ok 4now
 
 		currentLine = "A"
 
-		lyricVerse = [] # Store the final chosen verses here
+		lyricVerse = [] # Store the final chosen verse here
 
 		while ( len(lyricVerse) < 4 ) : # Loop until we have 4 lines
 
 			lyricSearchCount = 0
-	
 			lyricLine = ""
 			rhymeLine = ""
 	
-			while (not (lyricLine and rhymeLine)): # Loop until we have found viable lyricLine and associated rhymeLine
+			while (not (lyricLine and rhymeLine)): # Loop until we have found a viable lyricLine and associated rhymeLine
 
 				lyricSearchCount += 1
 				if (lyricSearchCount > lyricSearchLimit):
@@ -205,47 +219,45 @@ if __name__ == "__main__":
 						# This candidate is in meter, consider it
 						lyricLineCandidatesInMeter.append(lyricLineCandidate[0])
 
-				# print("DEBUG - ", currentLine, "line: Considering line candidates:",lyricLineCandidatesInMeter)
-
-				if lyricLineCandidatesInMeter:
-					lyricLine = random.choice(lyricLineCandidatesInMeter) # Pick a random candidate lyric line
+				if lyricLineCandidatesInMeter: # If any viable lines that match the meter are found...
+					lyricLine = random.choice(lyricLineCandidatesInMeter) # Pick a random candidate lyric line from the viable list
 					lyricLineLastWord = lyricLine.split()[-1]
 
-					candidateRhymeList = [] # We'll store some rhyme candidates in here for choosing
+					candidateRhymeList = [] # A place for some rhyme word candidates
 
-					for candidateRhyme in lyricDict[lyricLineLastWord][1][0][0]: # Loop through each potential rhyme
-						if candidateRhyme in lyricDict:
-							# print("DEBUG - ", currentLine, "line: - Found rhyme words existing in the lyricDict:", candidateRhyme)
-							# Add it to the candidate rhyme word list, unless it's exactly the same word
-							if not (candidateRhyme == lyricLineLastWord):
+					for candidateRhyme in lyricDict[lyricLineLastWord][1][0][0]: # Loop through each potential rhyme word
+						if candidateRhyme in lyricDict: # And see if we have any sentences that end in that word
+							# If so, add the word to the candidate rhyme word list
+							if not (candidateRhyme == lyricLineLastWord): #... unless it's exactly the same as the lastword already
 								candidateRhymeList.append(candidateRhyme)
 
-					if candidateRhymeList:
+					if candidateRhymeList: # If any viable rhyme words are found...
 						# Dedup and choose a random potential rhyme word
 						chosenRhyme = random.choice(list(dict.fromkeys(candidateRhymeList)))
-						# print("DEBUG - ", currentLine, "line: - Chosen rhyme word:",chosenRhyme)
 
-						# Find a list of lines that have the same lastWord that rhymes
+						# Retrive the list of lines that share a lastWord that rhymes
+						# Already know they exist from checking above
 						chosenRhymeLyricLineCandidates = lyricDict[chosenRhyme][0]
-						# print("DEBUG - ", currentLine, "line: - - Rhyming line candidates:",chosenRhymeLyricLineCandidates)
 
-						chosenRhymeLyricLineCandidatesInMeter = []
+						chosenRhymeLyricLineCandidatesInMeter = [] # A place for some candidate rhyming lines which also match meter
 
-						# loop through and find candidate lines that match this meter
+						# loop through and find candidate lines that match this meter.  Every line in this list already rhymes
 						for chosenRhymeLyricLineCandidate in chosenRhymeLyricLineCandidates:
 							if ((chosenRhymeLyricLineCandidate[1] > songMeter[currentLine] - songMeterPadding) and (chosenRhymeLyricLineCandidate[1] < songMeter[currentLine] + songMeterPadding)):
 								# Add them to the candidate list unless they're exactly the same line
 								if not (chosenRhymeLyricLineCandidate[0] == lyricLine) :
 									chosenRhymeLyricLineCandidatesInMeter.append(chosenRhymeLyricLineCandidate[0])
 
-						if chosenRhymeLyricLineCandidatesInMeter:
-							rhymeLine = random.choice(chosenRhymeLyricLineCandidatesInMeter)
+						if chosenRhymeLyricLineCandidatesInMeter: # If we have a list of lines that match meter and rhyme...
+							rhymeLine = random.choice(chosenRhymeLyricLineCandidatesInMeter) # Choose one
 
-			lyricVerse.append(lyricLine)
-			lyricVerse.append(rhymeLine)
+			lyricVerse.append(lyricLine) # Append the seed line
+			lyricVerse.append(rhymeLine) # Append the next line that matches rhyme and meter
 
-			if (currentLine == "A"): currentLine = "B"
+			if (currentLine == "A"): currentLine = "B" # Move from A to B line now
 			else: currentLine = "A"
+
+		# At this point, Verse contains a lyric in the format AABB
 
 		Verse = [lyricVerse[0], lyricVerse[2], lyricVerse[1], lyricVerse[3]] # Put it in ABAB
 
